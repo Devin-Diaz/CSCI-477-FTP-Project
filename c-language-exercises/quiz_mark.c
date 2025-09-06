37f2420f42b1a86d788dec19a65d933822cd35a2
Devin-Diaz <diazdevin7@outlook.com>
2025-09-05 21:38:41 -0400

    adding files

---
+++ c-language-exercises/quiz_mark.c
@@ -0,0 +1,11 @@
+#include <stdio.h>		
+
+int main () {
+	int	quiz1Mark;
+
+    printf("Enter quiz score: ");
+	scanf("%d", &quiz1Mark);
+    printf("Quiz score entered: %d\n", quiz1Mark);
+
+	return 0;
+}
